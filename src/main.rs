f0be18df7025b6a47ead05bd63082068eeefba2b
Nullscry <nullscry@nullscry.com>
2023-01-04 15:16:59 +0300

    Start match statement for argparsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-// use rusqlite::NO_PARAMS;
+use rusqlite::NO_PARAMS;
 use rusqlite::{Connection, Result};
+use std::process::Command;
 use std::{fs, io::Error, io::ErrorKind, path};
 
-fn parse_args(args: Vec<String>) -> Result<(), Error> {
+// fn split_args(args: Vec<String>) -> Result<(String, Vec<String>), Error> {
+//     let first = match args.pop() {
+//         Some(v) => v,
+//         None => return Err(Error::new(ErrorKind::Other, "Usage")),
+//     };
+
+//     Ok((first, args))
+// }
+
+fn parse_args(args: Vec<String>, conn: Connection) -> Result<(), Error> {
     if args.len() == 0 {
         return Err(Error::new(ErrorKind::Other, "Usage"));
     }
 
+    // args.reverse();
+
+    let cmd = args[0].as_str();
+    let num = &args[2];
+
     match &args[..] {
-        [command, rest @ ..] => {
-            println!("command found {} with args {:?}", command, rest);
-        }
+        [cmd, rest @ ..] => match cmd.as_str() {
+            "list" => println!("Command list with args {:?}", rest),
+            "create" => println!("Command create with args {:?}", rest),
+            "delete" => println!("Command delete with args {:?}", rest),
+            _ => eprintln!("Unknown command"),
+        },
         _ => {
             return Err(Error::new(ErrorKind::Other, "Usage"));
         }
@@ -31,7 +49,7 @@ fn main() -> Result<()> {
     let conn = Connection::open(db_path)?;
 
     let args: Vec<String> = std::env::args().skip(1).collect();
-    match parse_args(args) {
+    match parse_args(args, conn) {
         Ok(_v) => {
             return Ok(());
         }
